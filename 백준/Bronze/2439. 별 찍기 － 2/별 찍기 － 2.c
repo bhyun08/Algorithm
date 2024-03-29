97a950798da34619b0bc1b435f252400b3d91279
나병현 <162984395+bhyun08@users.noreply.github.com>
2024-03-29 16:18:44 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	int a;
+	scanf("%d",&a);
+	for(int i = 1; i <= a; i++)
+	{
+		for(int b = a-i; b > 0; b--)
+		{
+			printf(" ");
+		}
+		for(int j = 1; j <= i; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+} 
