6f3a79fbfb8d67b7ed96a6245d4f054fad5671e9
나병현 <162984395+bhyun08@users.noreply.github.com>
2024-04-22 11:02:03 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	printf("  ___  ___  ___\n");
+	printf("  | |__| |__| |\n");
+	printf("  |           |\n");
+	printf("   \\_________/\n");
+	printf("    \\_______/\n");
+	printf("     |     |\n");
+	printf("     |     |\n");
+	printf("     |     |\n");
+	printf("     |     |\n");
+	printf("     |_____|\n");
+	printf("  __/       \\__\n");
+	printf(" /             \\\n");
+	printf("/_______________\\\n");
+}
