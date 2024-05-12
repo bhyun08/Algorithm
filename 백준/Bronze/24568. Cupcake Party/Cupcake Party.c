a365bb0755fd760146d6d6b2d39b5c33affa2c17
나병현 <162984395+bhyun08@users.noreply.github.com>
2024-05-12 14:04:36 +0900

    [Bronze V] Title: Cupcake Party, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/24568. Cupcake Party/Cupcake Party.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int big,small;
+    scanf("%d",&big);
+    scanf("%d",&small);
+    int sum = big*8 + small*3;
+    printf("%d",sum - 28);
+    return 0;
+}
