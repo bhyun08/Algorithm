d5b8bf82451938afea0069578282060182e549b3
나병현 <162984395+bhyun08@users.noreply.github.com>
2024-05-14 08:31:53 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int a,b;
+    cin>>a;
+    for(int i=0;i<a;i++)
+    {
+        cin>>b;
+        cout<<b<<" "<<b<<"\n";
+    }
+    return 0;
+}
