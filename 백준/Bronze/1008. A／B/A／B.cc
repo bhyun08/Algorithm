58486d6018bc2110d36a7e9742797464876b7e17
나병현 <162984395+bhyun08@users.noreply.github.com>
2024-05-14 15:42:48 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int a,b;
+    cin>>a>>b;
+    cout << fixed;
+    cout.precision(9);
+    cout<<(double)a/b;
+    return 0;
+}
