7ba770f8ee04411f28229d99c074dacb6d13e720
나병현 <162984395+bhyun08@users.noreply.github.com>
2024-05-14 15:37:00 +0900

    [Bronze V] Title: ゾロ目 (Same Numbers), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27324. ゾロ目 （Same Numbers）/ゾロ目 （Same Numbers）.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+    
+    char a,b;
+	cin >> a >> b;
+	if (a == b) cout << 1;
+	else cout << 0;
+}
