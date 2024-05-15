fbc38110063db74b97145251bccdd00f2db46b4c
나병현 <162984395+bhyun08@users.noreply.github.com>
2024-05-15 22:15:41 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    string a;
+    cin>>a;
+    if(a=="SONGDO") cout<<"HIGHSCHOOL";
+    if(a=="CODE") cout<<"MASTER";
+    if(a=="2023") cout<<"0611";
+    if(a=="ALGORITHM") cout<<"CONTEST";
+    return 0;
+}
