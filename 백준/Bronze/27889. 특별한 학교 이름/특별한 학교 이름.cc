2d689e58e895fea80b74411e738dbd5efcf741a1
나병현 <162984395+bhyun08@users.noreply.github.com>
2024-05-15 21:20:18 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    string a;
+    cin>>a;
+    if(a=="NLCS") cout<<"North London Collegiate School";
+    if(a=="BHA") cout<<"Branksome Hall Asia";
+    if(a=="KIS") cout<<"Korea International School";
+    if(a=="SJA") cout<<"St. Johnsbury Academy";
+    return 0;
+}
