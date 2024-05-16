b1879e5ea52f800b0bed22b2f018e6a287451ddf
나병현 <162984395+bhyun08@users.noreply.github.com>
2024-05-16 17:26:14 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int w,h;
+    cin>>w>>h;
+    cout << fixed;
+    cout.precision(1);
+    cout<<(double)w*h*0.5;
+    return 0;
+}
