c10273795ce5312071e0d865eac3e43aca9e1a53
나병현 <162984395+bhyun08@users.noreply.github.com>
2024-05-17 23:00:07 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio;
+    cin.tie(0);
+    
+    int n;
+    cin>>n;
+    if(n==1) cout<<"12 1600";
+    else if(n==2) cout<<"11 894";
+    else if(n==3) cout<<"11 1327";
+    else if(n==4) cout<<"10 1311";
+    else if(n==5) cout<<"9 1004";
+    else if(n==6) cout<<"9 1178";
+    else if(n==7) cout<<"9 1357";
+    else if(n==8) cout<<"8 837";
+    else if(n==9) cout<<"7 1055";
+    else if(n==10) cout<<"6 556";
+    else if(n==11) cout<<"6 773";
+    return 0;
+}
