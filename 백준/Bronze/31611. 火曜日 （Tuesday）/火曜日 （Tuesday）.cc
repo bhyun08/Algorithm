50524c119536c989cfcafd04ea16070b72f426e1
나병현 <162984395+bhyun08@users.noreply.github.com>
2024-05-18 23:22:18 +0900

    [Bronze V] Title: 火曜日 (Tuesday), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31611. 火曜日 （Tuesday）/火曜日 （Tuesday）.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int a;
+    cin>>a;
+    if(a%7==2) cout<<"1";
+    else cout<<"0";
+    return 0;
+}
