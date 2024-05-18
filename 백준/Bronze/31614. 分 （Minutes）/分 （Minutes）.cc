5dfcf6972a578e8331e5eb934fc39cc95ff74e66
나병현 <162984395+bhyun08@users.noreply.github.com>
2024-05-18 23:18:25 +0900

    [Bronze V] Title: 分 (Minutes), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31614. 分 （Minutes）/分 （Minutes）.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int h,m;
+    cin>>h>>m;
+    cout<<h*60+m;
+    return 0;
+}
