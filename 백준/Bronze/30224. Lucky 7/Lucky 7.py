489b6ac9d614a676e55bf9a69a1ede9540ac29d2
나병현 <162984395+bhyun08@users.noreply.github.com>
2024-05-19 22:18:47 +0900

    [Bronze V] Title: Lucky 7, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/30224. Lucky 7/Lucky 7.py
@@ -0,0 +1,5 @@
+n = int(input())
+if "7" not in str(n) and n % 7 !=0: print(0)
+elif "7" not in str(n) and n % 7 == 0: print(1)
+elif "7" in str(n) and n % 7 !=0: print(2)
+elif "7" in str(n) or n % 7 == 0: print(3)
