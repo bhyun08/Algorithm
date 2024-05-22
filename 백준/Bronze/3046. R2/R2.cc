8b1110b235333722fbb7c582343a8c0ed12aab6b
나병현 <162984395+bhyun08@users.noreply.github.com>
2024-05-22 17:02:09 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int r1,r2,s;
+    cin>>r1>>s;
+    s=s*2;
+    r2=s-r1;
+    cout<<r2;
+    return 0;
+}
