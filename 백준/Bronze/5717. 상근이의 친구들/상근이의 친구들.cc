9444ad4540125296c881077b1277be5c223bc8c2
나병현 <162984395+bhyun08@users.noreply.github.com>
2024-05-23 08:18:07 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    while(1){
+    int x,y;
+    cin>>x>>y;
+    if(x==0&&y==0) break;
+    cout<<x+y<<"\n";
+    }
+    return 0;
+}
