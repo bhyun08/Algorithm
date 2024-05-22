61f038b4cb80f7dcdae4c471cdd9d91501b07398
나병현 <162984395+bhyun08@users.noreply.github.com>
2024-05-22 16:34:27 +0900

    [Bronze IV] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n;
+    cin>>n;
+    for(int i=n; i>0; i--)
+    {
+        cout<<i<<"\n";
+    }
+    return 0;
+}
