e3ba3be432f572e7fb69dcde3cf9159039e5ef7a
나병현 <162984395+bhyun08@users.noreply.github.com>
2024-05-23 08:20:14 +0900

    [Bronze IV] Title: Next in line, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int big,small;
+    cin>>small>>big;
+    cout<<(big-small)+big;
+    return 0;
+}
