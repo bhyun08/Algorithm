9df5cfdc5cd96501e9c01271eb276222e5a9ec81
나병현 <162984395+bhyun08@users.noreply.github.com>
2024-06-05 08:34:51 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=0; j<i; j++)
+        {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    for(int i=1; i<n; i++)
+    {
+        for(int j=0; j<n-i; j++)
+        {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
